e2ded3c47eb8692c05b0744169529ccc1a312810
Szczepan Faber <szczepiq@gmail.com>
2011-11-23 01:48:45 +0100

    Added initial classes used for the exercise

---
+++ src/main/java/org/thirdparty/OnlineTranslator.java
@@ -0,0 +1,12 @@
+package org.thirdparty;
+
+public class OnlineTranslator {
+
+    public static String translate(String word) {
+        //imagine that...
+        //it goes to the web and translates words
+        //it often fails due to online service not available
+        return "someTranslation";
+    }
+
+}
---
+++ src/test/java/org/workshop/SmartDictionaryTest.java
@@ -0,0 +1,10 @@
+package org.workshop;
+
+import org.junit.Test;
+
+public class SmartDictionaryTest {
+
+    @Test public void shouldTranslate() {
+        //the starting point
+    }
+}
